198a91a84787164cabc523a784d89e7585a33c18
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2024-07-31 22:05:19 -0300

    Update index.jsx

--- front/front/src/pages/cadastro/index.jsx
+++ front/front/src/pages/cadastro/index.jsx
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom"
 function Cadastro() {
 
     return(
-        <div className="mx-auto mt-10 bg-white p-8 border border-gray-300 rounded-lg shadow-lg">
+        <div className="max-w-md mx-auto mt-10 bg-white p-8 border border-gray-300 rounded-lg shadow-lg">
             <h2 className="text-2xl font-bold mb-6 text-center text-graw-800">Cadastro</h2>
-            <form >
+            <form className="flex flex-col gap-5">
                 <input placeholder="Nome" type="text"className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none" />
                 <input placeholder="Email" type="email" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none" />
                 <input placeholder="Senha" type="password" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none"/>
                 <button className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-400">Cadastrar-se</button>
             </form>
-            <Link to="/login">Já tem uma onta? Faça login</Link>
+            <Link to="/login" className="text-blue-700 hover:underline text-center mt-4 block ">Já tem uma onta? Faça login</Link>
         </div>
     )
 }
