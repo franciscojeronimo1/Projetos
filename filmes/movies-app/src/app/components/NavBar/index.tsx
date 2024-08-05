5e5096993aec2ae422b0e25d55ab1a61dd807512
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2024-08-05 11:32:20 -0300

    clear

--- filmes/movies-app/src/app/components/NavBar/index.tsx
+++
@@ -1,9 +0,0 @@
-import './index.scss'
-
-export default function NavBar() {
-    return (
-        <nav className="navbar">
-            <h1 className="page-title">filmes</h1>
-        </nav>
-    )
-}
