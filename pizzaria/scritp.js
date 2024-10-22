07b25725845d0fa6fd824a17cbb3914ebcbeb432
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2024-10-22 16:58:54 -0300

    .

--- pizzaria/scritp.js
+++ pizzaria/scritp.js
@@ -175,20 +175,20 @@ checkoutBtn.addEventListener("click", function () {
     return `${item.name} (Qtd: ${item.quantity}) - R$ ${(item.price * item.quantity).toFixed(2)}`;
   }).join("\n");
 
-   if (!isOpen) {
-    Toastify({
-      text: "Ops o restaurante está fechado!",
-      duration: 3000,
-      close: true,
-      gravity: "top", // top or bottom
-      position: "right", // left, center or right
-      stopOnFocus: true, // Prevents dismissing of toast on hover
-      style: {
-        background: "#ef4444",
-      },
-    }).showToast()
-    return;
-  } 
+  /* if (!isOpen) {
+      Toastify({
+        text: "Ops o restaurante está fechado!",
+        duration: 3000,
+        close: true,
+        gravity: "top", // top or bottom
+        position: "right", // left, center or right
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "#ef4444",
+        },
+      }).showToast()
+      return;
+    } */
 
   if (cart.length === 0) return;
   if (addressInput.value === "") {
