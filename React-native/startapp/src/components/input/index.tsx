40134854b6e35c36fb434be8f5fc3aaa1d6a6c65
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2025-03-13 15:21:54 -0300

    .

--- React-native/startapp/src/components/input/index.tsx
+++
@@ -1,9 +0,0 @@
-import { TextInput, TextInputProps } from 'react-native';
-
-import {styles} from "./styles"
-
-export function Input({...rest}: TextInputProps) {
-return (
-  <TextInput style={styles.input} {...rest}/>
-)
-}
